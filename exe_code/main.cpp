c28cca27c3cb2b66dd566ae94d557a7e80cd7959
eltigro1999 <eltigrolearning@gmail.com>
2022-10-25 12:38:58 +0400

    Eelement buffer object (EBO) added. Two triangles in carcas mode

--- exe_code/main.cpp
+++ exe_code/main.cpp
@@ -14,14 +14,21 @@ const char* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main(){\n"
-"FragColor=vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"FragColor=vec4(1.0f, 1.f, 1.f, 1.f);\n"
 "}\n";
 
 float vertices[] = 
-{-0.5f, -0.5f, 0,
-  0.5f, -0.5f, 0,
-  0.0f,  0.5f, 0};
+{-0.5f, -0.5f, 0,   //left bottom
+  0.5f, -0.5f, 0,   //right bottom
+  0.5f,  0.5f, 0,   //right top
+ -0.5f,  0.5f, 0};  //left top
 
+unsigned int indices[] = {
+    0, 1, 2,
+    0, 2, 3
+};
+
+unsigned int EBO;
 unsigned int VAO;
 unsigned int VBO;
 unsigned int vertexShader;
@@ -63,11 +70,14 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     createShader(GL_VERTEX_SHADER, vertexShader, 1, vertexShaderSource);
     createShader(GL_FRAGMENT_SHADER, fragmentShader, 1,fragmentShaderSource);
     shaderProgram = glCreateProgram();
@@ -87,6 +97,7 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);  //Draw triangles in carcas mode
 
 
     // Цикл рендеринга
@@ -101,13 +112,14 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // glfw: обмен содержимым front- и back-буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteVertexArrays(1, &VAO);
 
  
