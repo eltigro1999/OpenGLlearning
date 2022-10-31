0fe17b007b4db48d0b916509ccfa46f3b86914bb
Edmon <eltigrolearning@gmail.com>
2022-10-31 20:47:41 +0400

    texture addd

--- exe_code/main.cpp
+++ exe_code/main.cpp
@@ -2,48 +2,59 @@
 #include <GLFW/glfw3.h>
 #include "stb_image.h"
 
-
 #include <iostream>
 
-const char* vertexShaderSource =
-"#version 330 core\n"
-"layout(location = 0) in vec3 aPos;\n"
-"layout(location = 1) in vec3 aColor;\n"
-"out vec3 vertexColor;\n"
-"void main() {\n"
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.f);\n"
-"vertexColor=vec3(gl_Position.x, gl_Position.y, gl_Position.z);\n"
-"}\n";
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout(location = 0) in vec3 aPos;\n"
+    "layout(location = 1) in vec3 aColor;\n"
+    "layout(location = 2) in vec2 aTexCoord;\n"
+    "out vec2 TexCoord;\n"
+    "out vec3 ourColor;\n"
+    "void main() {\n"
+    "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.f);\n"
+    "ourColor=aColor;\n"
+    "TexCoord = aTexCoord;\n"
+    "}\n";
 
-const char* fragmentShaderSource =
-"#version 330 core\n"
-"in vec3 vertexColor;\n"
-"out vec4 FragColor;\n"
-"void main(){\n"
-"FragColor=vec4(vertexColor, 1.f);\n"
-"}\n";
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "in vec2 TexCoord;\n"
+    "in vec3 ourColor;\n"
+    "out vec4 FragColor;\n"
+    "uniform sampler2D ourTexture;\n"
+    "void main(){\n"
+    "FragColor=texture(ourTexture, TexCoord);\n"
+    "}\n";
 
 float vertices[] = {
-    0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-   -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f
+    };
+
+unsigned int indices[] = {
+    0, 1, 2,
+    0, 2, 3
 };
 
 unsigned int VAO;
 unsigned int VBO;
+unsigned int EBO;
 unsigned int vertexShader;
 unsigned int fragmentShader;
 unsigned int shaderProgram;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
-void checkShader(const unsigned int& shader);
-void createShader(const GLenum& shaderType, unsigned int& shader, const int& linesAmount, const char*& shaderSource);
- 
+void checkShader(const unsigned int &shader);
+void createShader(const GLenum &shaderType, unsigned int &shader, const int &linesAmount, const char *&shaderSource);
+
 // Константы
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
- 
+
 int main()
 {
     // glfw: инициализация и конфигурирование
@@ -51,10 +62,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
- 
- 
+
     // glfw: создание окна
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLLearning", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLLearning", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -63,7 +73,7 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
- 
+
     // glad: загрузка всех указателей на OpenGL-функции
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -74,53 +84,91 @@ int main()
     createShader(GL_VERTEX_SHADER, vertexShader, 1, vertexShaderSource);
     createShader(GL_FRAGMENT_SHADER, fragmentShader, 1, fragmentShaderSource);
     shaderProgram = glCreateProgram();
-    
+
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    
+
     glLinkProgram(shaderProgram);
-    
+
     int success;
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         char infoLog[512];
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
     }
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
     glGenBuffers(1, &VBO);
+    
     glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
     glBindVertexArray(0);
 
 
-    int nrAttributes;
-    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-    std::cout << "Max nr attributes: " << nrAttributes << std::endl;
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("/home/ani/Edmon/cpp/opengl/ravesli/exe_code/container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
+
     // Цикл рендеринга
     while (!glfwWindowShouldClose(window))
     {
-        //user input
+        // user input
         processInput(window);
 
-        //Rendering
-        glClearColor(1.f, 1.f, 1.f, 1.f);
+        // Rendering
+        glClearColor(.5f, .5f, .5f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
         // glfw: обмен содержимым front- и back-буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -129,36 +177,40 @@ int main()
     glDeleteBuffers(1, &VBO);
     glDeleteVertexArrays(1, &VAO);
 
- 
     // glfw: завершение, освобождение всех ранее задействованных GLFW-ресурсов
     glfwTerminate();
     return 0;
 }
- 
-void processInput(GLFWwindow* window) {
+
+void processInput(GLFWwindow *window)
+{
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
 // glfw: всякий раз, когда изменяются размеры окна (пользователем или операционной системой), вызывается данная callback-функция
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    // Убеждаемся, что окно просмотра соответствует новым размерам окна. 
-	// Обратите внимание, высота окна на Retina-дисплеях будет значительно больше, чем указано в программе
+    // Убеждаемся, что окно просмотра соответствует новым размерам окна.
+    // Обратите внимание, высота окна на Retina-дисплеях будет значительно больше, чем указано в программе
     glViewport(0, 0, width, height);
 }
 
-void checkShader(const unsigned int& shader) {
+void checkShader(const unsigned int &shader)
+{
     int success;
     char infolog[512];
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetShaderInfoLog(shader, 512, NULL, infolog);
-        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infolog << std::endl;
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n"
+                  << infolog << std::endl;
     }
 }
 
-void createShader(const GLenum& shaderType, unsigned int& shader, const int& linesAmount, const char*& shaderSource) {
+void createShader(const GLenum &shaderType, unsigned int &shader, const int &linesAmount, const char *&shaderSource)
+{
     shader = glCreateShader(shaderType);
     glShaderSource(shader, linesAmount, &shaderSource, NULL);
     glCompileShader(shader);
